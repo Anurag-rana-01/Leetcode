1c5d0e2b1a65f8781d4a97ac249ce35bb038b351
Anurag-rana-01 <anuragc979@gmail.com>
2026-07-30 15:54:01 +0530

    Time: 0 ms (100%), Space: 21.9 MB (39.32%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,18 +3,16 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
 
+        int xor1 = 0;
+        int xor2 = 0;
         for(int i=0;i<=n;i++){
-            int f = 0;
-            for(int j=0;j<n;j++){
-                if(nums[j] == i){
-                    f = 1;
-                    break;
-                }
-            }
-            if(f == 0){
-                return i;
-            }
+            xor1 = xor1^i;
         }
-        return -1;
+
+        for(int i=0;i<n;i++){
+            xor2 = xor2 ^ nums[i];
+        }
+
+        return xor1^xor2;
     }
 };
