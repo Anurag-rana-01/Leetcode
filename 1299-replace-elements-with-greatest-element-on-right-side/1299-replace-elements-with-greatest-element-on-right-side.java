21ef360247cc8180748664cda4e96e6877243a10
Anurag-rana-01 <anuragc979@gmail.com>
2026-08-22 17:03:59 +0530

    Time: 1403 ms (6.66%), Space: 48.6 MB (98.3%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int max = 0;
+        for(int i=0;i<arr.length;i++){
+            if(i==arr.length-1){
+                arr[i] = -1;
+                break;
+            }
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j] > max){
+                    max = arr[j];
+                }
+            }
+            arr[i] = max;
+            max = 0;
+        }
+        return arr;
+    }
+}
